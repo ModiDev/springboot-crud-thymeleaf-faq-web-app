b02e18eea92dbf04510fcff16f108785be961c34
ModiDev <pavithramoditha95@gmail.com>
2021-09-12 22:48:35 +0530

    add faq service and controller

--- src/main/java/com/faq/demo/controller/UserController.java
+++ src/main/java/com/faq/demo/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.faq.demo.controller;
 
+import com.faq.demo.model.User;
 import com.faq.demo.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -18,5 +21,19 @@ public class UserController {
    public String viewHomePage(Model model){
        model.addAttribute("listUsers", userService.getAllUsers());
        return "index";
+   }
+   @GetMapping("/showNewUserFaqForm")
+   public String showNewUserFaqForm( Model model){
+        //model attribute for form data
+        User user = new User();
+        model.addAttribute("user", user);
+        return "new_user";
+   }
+   @PostMapping("/saveUserFaq")
+   public String SaveFaq(@ModelAttribute("user") User user){
+        //save user data to db
+
+        userService.saveUser(user);
+        return "redirect:/";
    }
 }
